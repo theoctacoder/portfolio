ae1a8f65d7de3cc04b4da52e0f26e90eff65a2c6
audibhavesh <audichyabhavesh33@gmail.com>
2025-02-23 14:12:02 +0530

    Progress bar added icon added

--- .cloudflare/build.sh
+++ .cloudflare/build.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-# Clone Flutter with specific version tag
-git clone https://github.com/flutter/flutter.git -b 3.27.4
-export PATH="$PATH:`pwd`/flutter/bin"
+# Check if Flutter exists
+if ! command -v flutter &> /dev/null; then
+    echo "Flutter not found. Installing Flutter..."
+    # Clone Flutter with specific version tag
+    git clone https://github.com/flutter/flutter.git -b 3.27.4
+    export PATH="$PATH:`pwd`/flutter/bin"
+else
+    echo "Flutter already installed"
+    flutter --version
+fi
 
 # Enable web and check setup
 flutter config --enable-web
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:portfolio/bloc/portfolio_bloc.dart';
 import 'package:portfolio/ui/portfolio_screen.dart';
-
+import 'dart:html' if (dart.library.html) 'dart:html';
 void main() {
   runApp(BlocProvider(
     create: (context) => PortfolioBloc(),
